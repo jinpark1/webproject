9d8c06e11b790fafed7fc67c4dc41fa5335d4ea0
unknown <jempsey@gmail.com>
2018-08-06 23:05:47 -0700

    lading page css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,37 +1,16 @@
 import React, { Component } from 'react';
 import './Home.css';
-// import backgroundImg from '../../images/starimg1.jpg';
-// import backGroundGrey from '../../images/backgroundgrey.jpg';
-// import nodeLogo from '../../images/nodejs5.png';
-// import postgresLogo from '../../images/postgresql.png';
-// import reactLogo from '../../images/reactlogo.png';
-// import javaScript2 from '../../images/logo-javascript.svg';
-// import heroku from '../../images/heroku.png';
-// import glow from '../../images/blackimageglow.jpg';
+
 
 class Home extends Component {
     render() {
         return (
             <div className="home">
-                <div className="home-top">
-                    {/* <div>HOME</div> */}
-                    {/* <img src={glow} alt="backGroundGrey" /> */}
-                </div>
-
-                <div className="home-description">
-                    <div class="home-description-centered">THE TECH FORUM</div>
-                    {/* <div className="home-description-text">Welcome to the tech forum, feel free to discuss anything.</div> */}
-                </div>
-                {/* <div className="home-skill-title">"Tech Forum," is a web application that allow users to discuss freely on different technologies.</div>
-                <div className="home-skill-container">
-                    <div className="home-skill">
-                        <img src={reactLogo} alt="starimg1"/>
-                        <img src={javaScript2} alt="starimg1"/>
-                        <img src={nodeLogo} alt="starimg1"/>
-                        <img src={heroku} alt="starimg1"/>
-                        <img src={postgresLogo} alt="starimg1"/>
+                <div className="home-main-photo">
+                    <div class="home-description-centered">
+                        <span>THE TECH FORUM</span>  
                     </div>
-                </div>              */}
+                </div>
             </div>
         );
     }
