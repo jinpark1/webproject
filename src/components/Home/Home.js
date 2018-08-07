6794ac6061cdd30bd6b2b9479b7b91118525a653
unknown <jempsey@gmail.com>
2018-08-07 01:36:41 -0700

    Style change for landing page.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,8 +7,19 @@ class Home extends Component {
         return (
             <div className="home">
                 <div className="home-main-photo">
-                    <div class="home-description-centered">
+                    <div class="home-description">
                         <span>THE TECH FORUM</span>  
+                    </div>
+                </div>
+                <div className="home-communities">
+                    <div className="home-communities-container">
+                        <div className="home-communities-top">
+                            <img src="https://res.cloudinary.com/jinpark1/image/upload/v1530772055/projectForum/feedback3.jpg" alt="images"/>
+                            <img src="https://res.cloudinary.com/jinpark1/image/upload/v1530772055/projectForum/feedback3.jpg" alt="images" />
+                        </div>
+                        <div className="home-communities-bot">
+                        
+                        </div>
                     </div>
                 </div>
             </div>
