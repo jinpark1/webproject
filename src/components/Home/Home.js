5c4894ad100fe7b6e9106798595881b520118c04
unknown <jempsey@gmail.com>
2018-08-08 02:22:44 -0700

    design on landing page.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -33,16 +33,20 @@ class Home extends Component {
                     </div>
                     <div className="home-communities-container">
                         <div className="home-communities-img">
-                            <Link to="/forum/support">
-                                <img className="home-communities-click" src="https://res.cloudinary.com/jinpark1/image/upload/v1530675735/projectForum/dtjzb2qhslg4a5pjcggm.jpg" alt="images"/>
-                                <div className="home-communities-text3">SUPPORT</div>
-                                <div className="home-communities-text3-details">Any Issues Post Here!</div>
-                            </Link>
-                            <Link to="/forum/hardware">
-                                <img className="home-communities-click" src="https://res.cloudinary.com/jinpark1/image/upload/v1530680083/projectForum/hardware4.jpg" alt="images"/>
-                                <div className="home-communities-text4">HARDWARE</div>
-                                <div className="home-communities-text4-details">Start the Discussion!</div>
-                            </Link>
+                            <div className="home-communities-img-one">
+                                <Link to="/forum/support">
+                                    <img className="home-communities-click" src="https://res.cloudinary.com/jinpark1/image/upload/v1530675735/projectForum/dtjzb2qhslg4a5pjcggm.jpg" alt="images"/>
+                                    <div className="home-communities-text3">SUPPORT</div>
+                                    <div className="home-communities-text3-details">Any Issues Post Here!</div>
+                                </Link>
+                            </div>
+                            <div className="home-communities-img-one">
+                                <Link to="/forum/hardware">
+                                    <img className="home-communities-click" src="https://res.cloudinary.com/jinpark1/image/upload/v1530680083/projectForum/hardware4.jpg" alt="images"/>
+                                    <div className="home-communities-text4">HARDWARE</div>
+                                    <div className="home-communities-text4-details">Start the Discussion!</div>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
