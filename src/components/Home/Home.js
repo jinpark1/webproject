dfbf5f369c475f38d28487a3d64bc12a7f32aa14
unknown <jempsey@gmail.com>
2018-08-09 00:52:49 -0700

    design change

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,107 @@ import { Link } from 'react-router-dom';
 
 
 class Home extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            active: false,
+            active2: false,
+            active3: false,
+            active4: false
+        }
+    }
+
+    changeStyle = () => {
+        this.setState({
+            active: true
+        })
+    }
+
+    originalStyle = () => {
+        this.setState({
+            active: false
+        })
+    }
+
+    changeStyle2 = () => {
+        this.setState({
+            active2: true
+        })
+    }
+
+    originalStyle2 = () => {
+        this.setState({
+            active2: false
+        })
+    }
+
+    changeStyle3 = () => {
+        this.setState({
+            active3: true
+        })
+    }
+
+    originalStyle3 = () => {
+        this.setState({
+            active3: false
+        })
+    }
+
+    changeStyle4 = () => {
+        this.setState({
+            active4: true
+        })
+    }
+
+    originalStyle4 = () => {
+        this.setState({
+            active4: false
+        })
+    }
+    
     render() {
+        let imageFloat = 'home-communities-click'
+        if (this.state.active) {
+            imageFloat += ' home-communities-active';
+        }
+
+        let imageFloat2 = 'home-communities-click'
+        if (this.state.active2) {
+            imageFloat2 += ' home-communities-active';
+        }
+
+        let textFloat = 'home-communities-text2'
+        if (this.state.active2) {
+            textFloat += ' text-float';
+        }
+
+        let detailsFloat = 'home-communities-text2-details'
+        if (this.state.active2) {
+            detailsFloat += ' text-float'
+        }
+
+        let imageFloat3 = 'home-communities-click'
+        if (this.state.active3) {
+            imageFloat3 += ' home-communities-active';
+        }
+
+        let textFloat3 = 'home-communities-text3'
+        if (this.state.active3) {
+            textFloat3 += ' text-float';
+        }
+
+        let detailsFloat3 = 'home-communities-text3-details'
+        if (this.state.active3) {
+            detailsFloat3 += ' text-float'
+        }
+
+        let imageFloat4 = 'home-communities-click'
+        if (this.state.active4) {
+            imageFloat4 += ' home-communities-active';
+        }
+        
+
         return (
             <div className="home">
                 <div className="home-main-photo">
@@ -17,16 +117,39 @@ class Home extends Component {
                         <div className="home-communities-img">
                             <div className="home-communities-img-one">
                                 <Link to="/forum">
-                                    <img className="home-communities-click" src="https://res.cloudinary.com/jinpark1/image/upload/v1530772055/projectForum/feedback3.jpg" alt="images"/>
-                                    <div className="home-communities-text">All Topics</div>
-                                    <div className="home-communities-text-details">Everything about Tech</div>
+                                    <img onMouseEnter={this.changeStyle} 
+                                        onMouseLeave={this.originalStyle} 
+                                        className={imageFloat} 
+                                        src="https://res.cloudinary.com/jinpark1/image/upload/v1530772055/projectForum/feedback3.jpg" 
+                                        alt="images"
+                                    />
+                                    <div onMouseEnter={this.changeStyle} 
+                                        onMouseLeave={this.originalStyle} 
+                                        className="home-communities-text">
+                                        <span>All Topics</span>
+                                        <br/>
+                                        <span className="home-communities-text-details">Everything about Tech</span>
+                                    </div>
                                 </Link>
                             </div>
                             <div className="home-communities-img-one">
                                 <Link to="/forum/os">
-                                    <img className="home-communities-click" src="https://res.cloudinary.com/jinpark1/image/upload/v1533692316/projectForum/os2.jpg" alt="images"/>
-                                    <div className="home-communities-text2">OS</div>
-                                    <div className="home-communities-text2-details">Discuss Everything OS</div>
+                                    <img onMouseEnter={this.changeStyle2} 
+                                        onMouseLeave={this.originalStyle2} 
+                                        className={imageFloat2} 
+                                        src="https://res.cloudinary.com/jinpark1/image/upload/v1533692316/projectForum/os2.jpg" 
+                                        alt="images"
+                                    />
+                                    <div onMouseEnter={this.changeStyle2} 
+                                        onMouseLeave={this.originalStyle2} 
+                                        className={textFloat}>
+                                        <span>OS</span>
+                                    </div>
+                                    <div onMouseEnter={this.changeStyle2} 
+                                        onMouseLeave={this.originalStyle2} 
+                                        className={detailsFloat}>
+                                        <span>Discuss Everything OS</span>
+                                    </div>
                                 </Link>
                             </div>
                         </div>
@@ -35,9 +158,23 @@ class Home extends Component {
                         <div className="home-communities-img">
                             <div className="home-communities-img-one">
                                 <Link to="/forum/support">
-                                    <img className="home-communities-click" src="https://res.cloudinary.com/jinpark1/image/upload/v1530675735/projectForum/dtjzb2qhslg4a5pjcggm.jpg" alt="images"/>
-                                    <div className="home-communities-text3">SUPPORT</div>
-                                    <div className="home-communities-text3-details">Any Issues Post Here!</div>
+                                    <img onMouseEnter={this.changeStyle3} 
+                                        onMouseLeave={this.originalStyle3} 
+                                        className={imageFloat3}  
+                                        src="https://res.cloudinary.com/jinpark1/image/upload/v1530675735/projectForum/dtjzb2qhslg4a5pjcggm.jpg" 
+                                        alt="images"
+                                    />
+                                    <div onMouseEnter={this.changeStyle3} 
+                                        onMouseLeave={this.originalStyle3} 
+                                        className={textFloat3}
+                                        className="home-communities-text3">
+                                        <span>SUPPORT</span>
+                                    </div>
+                                    <div onMouseEnter={this.changeStyle3} 
+                                        onMouseLeave={this.originalStyle3} 
+                                        className={detailsFloat3}>
+                                        <span>Any Issues Post Here!</span>
+                                    </div>
                                 </Link>
                             </div>
                             <div className="home-communities-img-one">
