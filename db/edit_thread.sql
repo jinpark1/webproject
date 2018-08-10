1ceddf21ccb0af1fa19e54483f98a6c3e4b1a17b
unknown <jempsey@gmail.com>
2018-08-10 02:55:53 -0700

    edit thread.

--- db/edit_thread.sql
+++ db/edit_thread.sql
@@ -1,6 +1,8 @@
 UPDATE thread 
 SET subject = ${subject}, content = ${content}, category = ${category}
 WHERE id = ${id};
-select * from thread where id = ${id};
+select users.*, thread.id as thread_id, thread.subject, thread.category, thread.content, thread.created as thread_created from thread
+join users on thread.users_id = users.id
+where thread.id = ${id};
 
 
